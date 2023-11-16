155df930b9511213cf66acb8b4c5bd6df8ccc3ab
John Lin <johnlinp@gmail.com>
2023-11-16 07:19:00 -0800

    Also shows the current items when showing the recommentations

--- show-recommendation-results.bash
+++ show-recommendation-results.bash
@@ -1,5 +1,10 @@
 source config.bash
 
+SHOW_ITEMS_CYPHER="
+    MATCH(n:${MAIN_NODE_LABEL} {id: '${TARGET_MAIN_NODE_ID}'})-[:${RELATIONSHIP_TYPE}]->(m:${ITEM_NODE_LABEL})
+    RETURN m.name as itemName;
+"
+
 SHOW_RECOMMENDATION_CYPHER="
     MATCH(n:${MAIN_NODE_LABEL} {id: '${TARGET_MAIN_NODE_ID}'})-[:${RELATIONSHIP_TYPE}]->(m:${ITEM_NODE_LABEL})
     WITH collect(id(m)) AS lovedItems
@@ -11,6 +16,9 @@ SHOW_RECOMMENDATION_CYPHER="
 "
 
 set -e
+
+echo "Showing current items for ${MAIN_NODE_LABEL}:${TARGET_MAIN_NODE_ID} from Neo4j database ${NEO4J_DOCKER_NAME}..."
+docker exec ${NEO4J_DOCKER_NAME} cypher-shell --user neo4j --password ${NEO4J_PASSWORD} "${SHOW_ITEMS_CYPHER}"
 
 echo "Showing Recommendation for ${MAIN_NODE_LABEL}:${TARGET_MAIN_NODE_ID} from Neo4j database ${NEO4J_DOCKER_NAME}..."
 docker exec ${NEO4J_DOCKER_NAME} cypher-shell --user neo4j --password ${NEO4J_PASSWORD} "${SHOW_RECOMMENDATION_CYPHER}"
